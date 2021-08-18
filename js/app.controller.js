e94ca6917d50dcf2d115bb919736954a53284d83
tomer morad <tomer.morad771@gmail.com>
2021-08-18 15:05:18 +0300

    add location when click on map

--- js/app.controller.js
+++ js/app.controller.js
@@ -12,7 +12,11 @@ function onInit() {
         .then(() => {
             console.log('Map is ready');
         })
+        .then(() => {
+            clickMap()
+        })
         .catch(() => console.log('Error: cannot init map'));
+
 }
 
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
@@ -51,4 +55,32 @@ function onGetUserPos() {
 function onPanTo() {
     console.log('Panning the Map');
     mapService.panTo(35.6895, 139.6917);
+}
+
+function clickMap() {
+    const myLatlng = { lat: 32.0749831, lng: 34.9120554 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: myLatlng,
+    });
+    // Create the initial InfoWindow.
+    let infoWindow = new google.maps.InfoWindow({
+        content: "Click the map to get Lat/Lng!",
+        position: myLatlng,
+    });
+    infoWindow.open(map);
+    // Configure the click listener.
+    map.addListener("click", (mapsMouseEvent) => {
+        // Close the current InfoWindow.
+        infoWindow.close();
+        // Create a new InfoWindow.
+        infoWindow = new google.maps.InfoWindow({
+            position: mapsMouseEvent.latLng,
+        });
+        infoWindow.setContent(
+            JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+        );
+        infoWindow.open(map);
+        console.log(infoWindow.content);
+    });
 }
