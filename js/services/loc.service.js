e2e5410dada7a9cc9624d75448bc65a53e98ce4c
תומי ירמיה <tommyirmia@gmail.com>
2021-08-18 18:07:00 +0300

    added formated name

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -39,8 +39,9 @@ function createLoc(id, name, lat, lng) {
 function getLoc(lat, lng, cb) {
     const prm = axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${placeKey}`)
     prm.then(res => {
+        console.log(res.data.results[0].formatted_address);
         const placeId = res.data.results[0].place_id;
-        const placeName = res.data.results[0].address_components[0].long_name;
+        const placeName = res.data.results[0].formatted_address;
         createLoc(placeId, placeName, lat, lng);
         cb(gLocs);
     })
