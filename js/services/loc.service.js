a5969008c733182ea30cba510d474f19201b1749
תומי ירמיה <tommyirmia@gmail.com>
2021-08-18 16:04:39 +0300

    added storage and DB

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,19 +1,37 @@
+import { storageService } from './storage.service.js'
+
+
 export const locService = {
-    getLocs
+    getLocs,
+    createLoc
 }
 
 
-const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
+const gLocs = [
+    createLoc('Greatplace', 32.047104, 34.832384),
+    createLoc('Neveragain', 32.047201, 34.832581)
 ]
 
 function getLocs() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve(locs);
+            resolve(gLocs);
         }, 2000)
     });
 }
 
+function createLoc(name, lat, lng) {
+    return {
+        id: _createId(),
+        name,
+        lat,
+        lng,
+        weather: 'cloudy',
+        createdAt: new Date(),
+        updatedAt: 'hasnt been updated'
+    }
+}
 
+function _createId() {
+    return Math.random().toString(36).substr(2, 9);
+}
---
+++ js/services/storage.service.js
@@ -0,0 +1,16 @@
+export const storageService = {
+        save: saveToStorage,
+        load: loadFromStorage
+    }
+    // technique for debug from console:
+    // window.storush = storageService
+
+
+function saveToStorage(key, val) {
+    localStorage.setItem(key, JSON.stringify(val))
+}
+
+function loadFromStorage(key) {
+    const json = localStorage.getItem(key)
+    return JSON.parse(json)
+}
